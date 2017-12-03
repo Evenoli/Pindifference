dffcb2b9ff4bd3dbfe7dbd1bf4b2133108974da7
Oliver <evenoli@hotmail.co.uk>
2017-12-03 16:02:53 +0000

    First commit. Basic table with flippers!

---
+++ Assets/Scripts/FlipperControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperControl : MonoBehaviour {
+
+    public HingeJoint hinge;
+
+    public float restPos = 0f;
+    public float pressedPos = 60f;
+    public float hitStrength = 10000f;
+    public float flipperDamper = 150f;
+
+    public string inputName;
+
+    // Use this for initialization
+    void Start () {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+
+        if(Input.GetAxis(inputName) == 1)
+        {
+            spring.targetPosition = pressedPos;
+        }
+        else
+        {
+            spring.targetPosition = restPos;
+        }
+
+        hinge.spring = spring;
+    }
+
+}
---
+++ Assets/Scripts/LauncherControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherControl : MonoBehaviour {
+
+    public string inputName;
+    public int m_springPower;
+    public int m_springDamper;
+    public SpringJoint m_springJoint;
+    public float m_MaxPullback;
+    public float m_pullbackSpeed;
+
+    private float m_startZ;
+
+    // Use this for initialization
+    void Start () {
+        m_startZ = transform.localPosition.z;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis(inputName) == 1)
+        {
+            m_springJoint.spring = 0;
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z - m_pullbackSpeed);
+        }
+        else
+        {
+            m_springJoint.spring = m_springPower;
+        }
+
+    }
+}
