fc0650bd6e68248161033950c0a27be186f296fa
Oliver <evenoli@hotmail.co.uk>
2017-12-05 21:44:31 +0000

    Added bumper physics and some lighting for them

--- Assets/Bumper.cs
+++ Assets/Bumper.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Bumper : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float bumperPowerScale;
+    public float maxMagnitude;
+    public float minBumperPower;
+
+    public Color flashCol;
+    private Color initialCol;
+    public float flashResetRate;
+    private bool isFlashing;
+    private float flashLerpFactor;
+    private Material mat;
+
+    // Use this for initialization
+    void Start () {
+        isFlashing = false;
+        mat = gameObject.GetComponent<Renderer>().material;
+        initialCol = mat.color;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(isFlashing)
+        {
+            flashLerpFactor += flashResetRate;
+            mat.color = Color.Lerp(flashCol, initialCol, flashLerpFactor);
+        }
 	}
 
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject obj = collision.collider.gameObject;
+        if(obj.tag == "Ball")
+        {
+            Rigidbody ballRigidBdy = obj.GetComponent<Rigidbody>();
+            ContactPoint cp = collision.contacts[0];
+            Vector3 reverseVelocity = collision.relativeVelocity;
+            Vector3 bounce = Vector3.Reflect(reverseVelocity, cp.normal);
+            Vector3 bounceDirection = bounce.normalized;
+            ballRigidBdy.velocity = Vector3.ClampMagnitude((bounce * bumperPowerScale + bounceDirection * minBumperPower), maxMagnitude);
+            Flash();
+        }
+    }
+
+    public void Flash()
+    {
+        mat.color = flashCol;
+        isFlashing = true;
+        flashLerpFactor = 0;
+    }
 
 }
