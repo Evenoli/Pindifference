2d7463c89937e1bc046500b916f1456c9b155658
Oliver <evenoli@hotmail.co.uk>
2017-12-07 15:55:50 +0000

    Added Panels, tweaked angle and flipper force

---
+++ Assets/Panel.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour {
+
+    private bool m_up;
+    public float m_dropSpeed;
+    private bool m_moving;
+    public float m_dropHeight;
+
+    private float m_minHeight;
+    private float m_maxHeight;
+
+    private BoxCollider m_collider;
+
+    public Color flashCol;
+    private Color initialCol;
+    public float flashResetRate;
+    private bool isFlashing;
+    private float flashLerpFactor;
+    private Material mat;
+
+    private PointManager m_pointMan;
+
+    // Use this for initialization
+    void Start () {
+        m_up = true;
+        m_moving = false;
+        isFlashing = false;
+
+        m_maxHeight = transform.localPosition.y;
+        m_minHeight = m_maxHeight - m_dropHeight;
+
+        m_collider = GetComponent<BoxCollider>();
+        mat = gameObject.GetComponent<Renderer>().material;
+        initialCol = mat.color;
+        m_pointMan = transform.root.gameObject.GetComponent<PointManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFlashing)
+        {
+            flashLerpFactor += flashResetRate;
+            mat.color = Color.Lerp(flashCol, initialCol, flashLerpFactor);
+        }
+
+        if(m_moving)
+        {
+            float y= transform.localPosition.y;
+
+            if (m_up)
+            {
+                y -= m_dropSpeed;
+                if (y <= m_minHeight)
+                {
+                    y = m_minHeight;
+                    m_up = false;
+                    m_collider.enabled = false;
+                    m_moving = false;
+                }
+            }
+            else
+            {
+                y += m_dropSpeed;
+                if (y >= m_maxHeight)
+                {
+                    y = m_maxHeight;
+                    m_up = true;
+                    m_collider.enabled = true;
+                    m_moving = false;
+                }
+            }
+
+            transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject obj = collision.collider.gameObject;
+        if (obj.tag == "Ball")
+        {
+            m_moving = true;
+            Flash();
+            m_pointMan.AddToScore(PointManager.InteractionType.PANEL);
+        }
+    }
+
+    public bool isDown()
+    {
+        return !m_up;
+    }
+
+    public void Reset()
+    {
+        if (!m_up)
+            m_moving = true;
+    }
+
+    public void Flash()
+    {
+        mat.color = flashCol;
+        isFlashing = true;
+        flashLerpFactor = 0;
+    }
+}
---
+++ Assets/PanelCluster.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelCluster : MonoBehaviour {
+
+    private PointManager m_pointMan;
+
+    public Panel[] panels;
+
+    // Use this for initialization
+    void Start () {
+        m_pointMan = transform.root.gameObject.GetComponent<PointManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        bool allPanelsDown = true;
+
+		foreach(Panel p in panels)
+        {
+            if (!p.isDown())
+                allPanelsDown = false;
+        }
+
+        if(allPanelsDown)
+        {
+            m_pointMan.IncreaseMultiplyer();
+            foreach (Panel p in panels)
+            {
+                p.Reset();
+            }
+        }
+	}
+}
