97935d07147f346549af451e1f5f947d55965a7b
Oliver <evenoli@hotmail.co.uk>
2017-12-13 19:32:49 +0000

    Added corner bumpers

---
+++ Assets/CornerBumper.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerBumper : MonoBehaviour {
+
+    public float bumperPowerScale;
+    public float maxMagnitude;
+    public float minBumperPower;
+
+    public Color flashCol;
+    private Color initialCol;
+    public float flashResetRate;
+    private bool isFlashing;
+    private float flashLerpFactor;
+    private Material mat;
+    private Material bodyMat;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        isFlashing = false;
+        mat = gameObject.GetComponent<Renderer>().material;
+        bodyMat = transform.parent.GetComponent<Renderer>().material;
+        initialCol = mat.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFlashing)
+        {
+            flashLerpFactor += flashResetRate;
+            mat.color = Color.Lerp(flashCol, initialCol, flashLerpFactor);
+            bodyMat.color = Color.Lerp(flashCol, initialCol, flashLerpFactor);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject obj = collision.collider.gameObject;
+        if (obj.tag == "Ball")
+        {
+            Rigidbody ballRigidBdy = obj.GetComponent<Rigidbody>();
+            ContactPoint cp = collision.contacts[0];
+            Vector3 reverseVelocity = collision.relativeVelocity;
+            Vector3 bounce = Vector3.Reflect(reverseVelocity, cp.normal);
+            Vector3 bounceDirection = cp.normal * Mathf.Max(1, bounce.magnitude/2);
+
+            ballRigidBdy.velocity = Vector3.ClampMagnitude((bounce * bumperPowerScale + bounceDirection * minBumperPower), maxMagnitude);
+            Flash();
+        }
+    }
+
+    public void Flash()
+    {
+        mat.color = flashCol;
+        bodyMat.color = flashCol;
+
+        isFlashing = true;
+        flashLerpFactor = 0;
+    }
+}
