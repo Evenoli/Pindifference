a0c075c1dccf7eb2f554bd3e56f6053795b71ed4
Oliver <evenoli@hotmail.co.uk>
2018-01-08 16:17:28 +0000

    Added some colouring and tubes, along with boost gates for tubve traversal

---
+++ Assets/Scripts/BoostGate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostGate : MonoBehaviour {
+
+    public float m_BoostSpeed;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+        if (obj.tag == "Ball")
+        {
+            obj.GetComponent<Rigidbody>().velocity += transform.TransformDirection(new Vector3(0, m_BoostSpeed, 0));
+
+        }
+    }
+}
