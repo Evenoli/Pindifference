59fa27e896021aac2604b64f0894e6794fe9d305
Oliver <evenoli@hotmail.co.uk>
2018-01-18 17:26:56 +0000

    Added skeleton for power announcer

---
+++ Assets/Scripts/PowerAnnouncer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerAnnouncer : MonoBehaviour {
+
+    public GameObject m_AnnounceText;
+
+	// Use this for initialization
+	void Start () {
+        m_AnnounceText.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AnnouncePower(PowerupManager.PowerUps power)
+    {
+
+    }
+}
